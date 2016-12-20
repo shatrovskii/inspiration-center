3dcea5fe7010301e0b9dec70e26b4abe762b7d6e
Dmitrii Shatrovskii <shatrovskii@realtimeboard.com>
2016-12-20 17:26:01 +0500

    debuggin deploy script

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,36 +1,31 @@
 #!/bin/bash
 set -ev
-exit 1
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 	echo "Travis should not deploy from pull requests"
   	exit 0
-fi
+else
+	if [ -n "$GITHUB_API_TOKEN" ]; then
+		REPO=$(git config remote.origin.url)
 
-if [ -n "$GITHUB_API_KEY" ]; then
-	echo "We have API key"
-	REPO=$(git config remote.origin.url)
+		if [ "$TRAVIS_BRANCH" == "$MASTER_BRANCH" ]; then 
+			mkdir -p $TARGET_DIR
+		else
+			mkdir -p $TARGET_DIR/$TRAVIS_BRANCH
+		fi
 
-	if [ "$TRAVIS_BRANCH" == "$MASTER_BRANCH" ]; then 
-		echo "This is master branch"
-		mkdir -p $TARGET_DIR
-	else
-		echo "This is not master branch"
-		mkdir -p $TARGET_DIR/$TRAVIS_BRANCH
+		cd $TRAVIS_BUILD_DIR
+		git clone -b ${TARGET_BRANCH} ${REPO} ${TARGET_DIR}
+		rsync -rt --delete $SOURCE_DIR/ $TARGET_DIR/
+		cd $TARGET_DIR
+		git init
+		git checkout -b $TARGET_BRANCH
+		git config user.name "travis"
+		git config user.email "travis"
+		git add -A .
+		git commit -m "Deploy to GitHub Pages"
+		OWNER=`dirname $TRAVIS_REPO_SLUG`
+		git push -f -q https://$OWNER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TARGET_BRANCH > /dev/null 2>&1
+		cd $TRAVIS_BUILD_DIR
 	fi
-
-	cd $TRAVIS_BUILD_DIR
-	git clone -b ${TARGET_BRANCH} ${REPO} ${TARGET_DIR}
-	rsync -rt --delete $SOURCE_DIR/ $TARGET_DIR/
-	cd $TARGET_DIR
-	git init
-	git checkout -b $TARGET_BRANCH
-	git config user.name "travis"
-	git config user.email "travis"
-	git add -A .
-	git commit -m "Deploy to GitHub Pages"
-	OWNER=`dirname $TRAVIS_REPO_SLUG`
-	git push -f -q https://$OWNER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TARGET_BRANCH > /dev/null 2>&1
-	cd $TRAVIS_BUILD_DIR
-	echo "The end"
 fi
