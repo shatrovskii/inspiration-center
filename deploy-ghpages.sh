b493dcd626b8535e8b9145068d45875824cc72f2
Dmitrii Shatrovskii <shatrovskii@realtimeboard.com>
2016-12-20 12:59:15 +0500

    deploy gh-pages

---
+++ deploy-ghpages.sh
@@ -0,0 +1,17 @@
+set -e
+
+TARGET_BRANCH="gh-pages"
+
+if [ -n "$GITHUB_API_KEY" ]; then
+	cd "$TRAVIS_BUILD_DIR"
+	cd dist
+	git init
+	git checkout -b $TARGET_BRANCH
+	git add .
+	git -c user.name='travis' -c user.email='travis'
+	git commit -m "Deploy to GitHub Pages"
+	IFS='/'
+	REPO_SLUG_ARR=($TRAVIS_REPO_SLUG)
+	unset IFS
+	git push -f -q https://$REPO_SLUG_ARR:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG gh-pages &2>/dev/null
+fi
